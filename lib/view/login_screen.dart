36919a17562064787a635d5c44f0a0ea49a59871
Yash2121m <yashspatil2005@gmail.com>
2026-03-27 11:43:07 +0530

    Named Routes

---
+++ lib/view/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_architecture/utils/routes/routes.dart';
+import 'package:mvvm_architecture/utils/routes/routes_names.dart';
+import 'package:mvvm_architecture/view/home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: (){
+            Navigator.pushNamed(context, RoutesNames.home);
+           // Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+          },
+          child: const Text("Click Here"),
+        ),
+      )
+    );
+  }
+}
